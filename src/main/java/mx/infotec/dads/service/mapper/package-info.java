2aaa46863a905872b506d4dc1fb0d79cb878a065
Kukulkan Team <suport@kukulkan.org.mx>
2018-07-03 18:15:30 -0500

    Firts version of project

--- src/main/java/mx/infotec/dads/service/mapper/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * MapStruct mappers for mapping domain objects and Data Transfer Objects.
- */
-package mx.infotec.dads.service.mapper;
--- src/main/java/mx/infotec/dads/web/rest/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring MVC REST controllers.
- */
-package mx.infotec.dads.web.rest;
